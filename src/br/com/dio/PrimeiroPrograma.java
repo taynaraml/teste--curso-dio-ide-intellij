7644d60bd075751469912abd2869496742358d90
taynaraml <taynaraml230304@gmail.com>
2022-05-09 11:14:31 -0300

    first commit intellij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello world!" + (a+b));
+    }
+}
